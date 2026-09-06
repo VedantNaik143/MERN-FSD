e1568bf6f5c8a91652b6294f9883be97e6b2ed79
VedantNaik143 <233334303+VedantNaik143@users.noreply.github.com>
2026-09-06 17:51:33 +0530

    Version control demo

---
+++ version-control-demo/calculator.test.js
@@ -0,0 +1,13 @@
+const { add, subtract, multiply } = require('./calculator');
+
+test('adds two numbers', () => {
+    expect(add(2, 3)).toBe(5);
+});
+
+test('subtracts two numbers', () => {
+    expect(subtract(5, 2)).toBe(3);
+});
+
+test('multiplies two numbers', () => {
+    expect(multiply(4, 3)).toBe(12);
+});
